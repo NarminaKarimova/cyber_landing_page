1225d58ac86bc504b73f05d447b680ecf6ffcf5b
NarminaKarimova <narmina.krmva@gmail.com>
2023-04-02 16:57:06 +0400

    adding down/up arrow animation

--- Page/index.js
+++ Page/index.js
@@ -1,11 +1,14 @@
+
 const modules = document.querySelectorAll('.module');
 
 modules.forEach((module) => {
     const details = module.querySelector('.details');
     const expandBtn = module.querySelector('.expand-btn');
+    const arrowImg = expandBtn.querySelector('.arrow');
 
     expandBtn.addEventListener('click', () => {
         details.classList.toggle('show');
-        expandBtn.textContent = details.classList.contains('show') ? 'Read Less' : 'Read More';
+        arrowImg.src = details.classList.contains('show') ? '/Resources/uparrow.png' : '/Resources/downarrow.png';
+        arrowImg.alt = details.classList.contains('show') ? '&uarr;' : '&darr;';
     });
 });
